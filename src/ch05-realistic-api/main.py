ad95513eaad916cc6ccd84b89d2ca5ee65164a2f
sarye.haddadi <sarye@live.fr>
2021-11-12 11:41:29 +0100

    [wip] ch05

--- src/ch05-realistic-api/main.py
+++ src/ch05-realistic-api/main.py
@@ -1,12 +1,25 @@
 import fastapi
-import uvicorn
+from fastapi.staticfiles import StaticFiles
 
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+import uvicorn
+from starlette.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent.resolve()
 
 app = fastapi.FastAPI()
+templates = Jinja2Templates(directory=f'{BASE_PATH}/templates')
+static_dir = f'{BASE_PATH}/static'
+app.mount('/static', StaticFiles(directory=static_dir))
 
-@app.get('/')
-def index():
-    return "Hello Weather app!"
+
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    print(templates.env)
+    return templates.TemplateResponse('home/index.html', {'request': request})
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
