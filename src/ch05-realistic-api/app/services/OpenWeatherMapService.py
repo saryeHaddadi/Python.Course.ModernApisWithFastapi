e64559f3f7570844d55898d446509781f4c8fd35
sarye.haddadi <sarye@live.fr>
2021-11-12 22:18:11 +0100

    svg

--- src/ch05-realistic-api/app/services/OpenWeatherMapService.py
+++ src/ch05-realistic-api/app/services/OpenWeatherMapService.py
@@ -2,6 +2,6 @@ from web.viewmodels.Location import Location
 
 def get_report(loc: Location, units: str) -> dict:
     q = f'{loc.city},{loc.country},{loc.state}'
-    apiKey = '123'
+    apiKey = 'bd518531a3af194f6054967388e2eefb'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={apiKey}&units={units}'
 
