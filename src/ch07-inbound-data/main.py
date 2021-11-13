c28c87c9cb76397a75afa363544b01d10302b211
sarye.haddadi <sarye@live.fr>
2021-11-13 20:23:23 +0100

    svg

--- src/ch07-inbound-data/main.py
+++ src/ch07-inbound-data/main.py
@@ -3,7 +3,6 @@ from web.StartupApp import StartupApp
 
 app = StartupApp().create_app()
 
-print(__name__)
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
 
